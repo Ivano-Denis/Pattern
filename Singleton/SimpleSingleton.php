a642d90a45ff2679f366fd9a7dcd8265293ed5c3
Ivano-Denis <64743369+Ivano-Denis@users.noreply.github.com>
2021-04-27 14:49:46 +0300

    Add files via upload

---
+++ Singleton/SimpleSingleton.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+class Singleton
+{
+
+    private static $instance;
+    public $item;
+
+    static public function getInstance()
+    {
+        return static::$instance ?? (static::$instance = new static());
+    }
+
+    private function __construct(){
+    }
+
+}
+
+$firstResult = Singleton::getInstance();
+$secondResult = Singleton::getInstance();
+
+$firstResult->item = 1;
+$secondResult->item = 2;
+
+print_r($firstResult->item); // 2
+
+print_r($secondResult->item);// 2
+
+print_r($firstResult === $secondResult); //true
+
+
