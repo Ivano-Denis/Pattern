d3aa2e04d44be12967d5bebc22a2c14a096b2ebe
Ivano-Denis <redisochko@gmail.com>
2021-05-03 18:49:06 +0300

    fix

--- Prototype/Prototype.php
+++ Prototype/Prototype.php
@@ -9,30 +9,6 @@ class monster
 
     }
 }
-class goblin
-{
-    public function getClone($spawn)
-    {
-        return clone $spawn;
-    }
-}
-class ork
-{
-    public function getClone($spawn)
-    {
-        return clone $spawn;
-    }
-}
+
 
 $monster = new monster();
-$monster1 = new goblin();
-$monster2 = new ork();
-
-
-$singleArray[] = $monster1->getClone($monster);
-$singleArray[] = $monster2->getClone($monster);
-
-$singleArray[0]->weapons = 'knife';
-$singleArray[1]->weapons = 'ax';
-
-var_dump($singleArray);
